f1fb1d1b346b2bd094b7f5db27190628d7a93852
andressatarina <andressa_silva03@hotmail.com>
2021-03-04 15:14:10 -0300

    ajuste da img

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <h1>Burguer do Zé</h1>
 Burguer do Zé<br>
 O Melhor Lanche da Região!<br>
-<img src="https://www.revistamenu.com.br/wp-content/uploads/2020/05/diadoburger-1050x580.jpg">
+<img src="https://www.revistamenu.com.br/wp-content/uploads/2020/05/diadoburger-1050x580.jpg" style="
+    width: 239px;
+    ">
